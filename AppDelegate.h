1d0e4b277fadaa2a874b788f88609ef80bbf05dc
徐志鹏 <1397567106@qq.com>
2017-11-06 00:54:40 +0800

    cash yixia

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestLable
+//
+//  Created by 徐志鹏 on 16/12/19.
+//  Copyright © 2016年 徐志鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
